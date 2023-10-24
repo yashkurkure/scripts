7afa6d742189aa99e51540f1b82e39c7d11c9a63
yashkurkure <yash.kurkure1903@gmail.com>
2023-10-23 20:30:13 -0600

    rsync to mounted drive

--- rsync/sync.sh
+++ rsync/sync.sh
@@ -3,5 +3,5 @@
 # Script syncs files from source to destination
 #
 #
-cd ~/common && pwd && rsync -avz -e ssh ./* yash@192.168.0.12:~/common
+cd ~/common && pwd && rsync -avz -e ssh ./* yash@192.168.0.12:~/drive/common
 pwd
